b554e286a161e68c8d95a0f75175a8e83f3bd3dc
mau <mauriciomamani306@gmail.com>
2023-09-08 15:54:24 -0500

    connect_mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
