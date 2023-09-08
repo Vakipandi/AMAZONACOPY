dd3745ea6ca3da82db48629e7eeb9e629eef9a15
mau <mauriciomamani306@gmail.com>
2023-09-08 16:53:40 -0500

    seed_sample_data

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import data from './data.js';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seed.routes.js';
+import productRouter from './routes/product.routes.js';
 
 dotenv.config();
 
@@ -19,26 +21,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
+app.use('/api/seed', seedRouter)
+app.use('/api/products', productRouter)
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
