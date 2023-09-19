01db782d29d2d4ec4489d704211aa3991214ae3f
mau <mauriciomamani306@gmail.com>
2023-09-18 20:31:33 -0500

    m

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,11 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -46,6 +50,6 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
