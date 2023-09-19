f2c0cb4fd32ea83fabce65a6e1c016b3a2444cb8
mau <mauriciomamani306@gmail.com>
2023-09-18 20:24:49 -0500

    m

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -34,7 +34,7 @@ function HomeScreen() {
     const fetchData = async () => {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
-        const result = await axios.get('http://localhost:5000/api/products');
+        const result = await axios.get('http://0.0.0.0:5000/api/products');
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: err.message });
