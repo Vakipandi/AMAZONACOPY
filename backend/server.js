25f655d22c1c0185e216a0e451794872064bb817
mau <mauriciomamani306@gmail.com>
2023-09-18 20:36:25 -0500

    asd

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,14 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: 'https://mern-amazon-app-tzxl.onrender.com/',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: 'https://mern-amazon-app-tzxl.onrender.com/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
